00414b5920f49d562b58333fc9e43f8f15ac8fcc
elvis-brandao <elvis.brandao@outlook.com>
2022-04-19 10:33:57 -0300

    Aplicando states e effects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// const logo = require('./logo.svg');
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import BemVindo from './components/BemVindo';
+import NavBar from './components/NavBar'
 
 function App() {
+  const caminhos = [
+    {
+      id: 1,
+      caminho: '/',
+      titulo: 'Página inicial'
+    },
+    {
+      id: 2,
+      caminho: '/perfil',
+      titulo: 'Perfil'
+    },
+    {
+      id: 3,
+      caminho: '/repositorios',
+      titulo: 'Repositórios'
+    },
+    {
+      id: 4,
+      caminho: '/busca',
+      titulo: 'Busca'
+    }
+  ];
+
+  const [num, setNum] = useState(1);
+  const aumentaNum = () => setNum(num + 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <NavBar menu={caminhos}  />
+      <h1>{num}</h1><button onClick={aumentaNum}>Clique</button>
+      <BemVindo meuNome="Léo"></BemVindo>
+      <BemVindo meuNome="Lígia"/>
+      <BemVindo />
+      <BemVindo />
+      <BemVindo />
+      <BemVindo />
     </div>
   );
 }
 
 export default App;
+//module.exports = App;
---
+++ src/components/BemVindo/index.js
@@ -0,0 +1,7 @@
+function BemVindo(props) {
+  return (
+    <h1>Bem-vindo ao site GitHub alternativo, {props.meuNome}</h1>
+  )
+}
+
+export default BemVindo;
